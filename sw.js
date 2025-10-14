50c1bc12508e9ae569104949b0021a3a65467575
arrojasboris-byte <ar.rojasboris@gmail.com>
2025-10-14 22:41:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('audioseguimiento-v5d')
+  e.waitUntil(caches.open('audioseguimiento-v5f')
     .then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
 });
 self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
