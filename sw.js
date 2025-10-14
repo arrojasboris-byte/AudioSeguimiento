26ed90651a87ad5ecf3aec607299b27bf1584891
arrojasboris-byte <ar.rojasboris@gmail.com>
2025-10-14 23:44:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-const CACHE_NAME = 'audioseguimiento-v5h'; // <-- cambia la versión si vuelves a actualizar
+const CACHE_NAME = 'audioseguimiento-v5i'; // ¡cambia la versión para invalidar caché!
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 
-self.addEventListener('install', e=>{
+self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
