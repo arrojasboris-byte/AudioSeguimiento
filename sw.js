659eb50758f84afd06236b8011e383cbbe430d11
arrojasboris-byte <ar.rojasboris@gmail.com>
2025-10-16 22:30:13 +0200

    Update GAS_URL to new Google Apps Script URL

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/xxxx/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxr7BFaMOT3pv0Ty-jnCA5zCC6046u-FPsdnIhS0xv8XU3krlGnDJnARhm_jeQ0U30/exec';
 // o
 const API_URL = '...';
 // o
